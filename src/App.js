83a721002c3b3985ee137c71813423eed68238ab
stefanog1121 <123034209+stefanog1121@users.noreply.github.com>
2023-11-07 15:22:35 -0500

    git commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const choices = ['rock', 'paper', 'scissors'];
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      playerChoice: null,
+      computerChoice: null,
+      result: '',
+      playerTally: 0,
+      computerTally: 0,
+      tieTally: 0
+    };
+  }
+
+  getRandomMove() {
+    var move = Math.floor(Math.random() *  choices.length);
+    return choices[move];
+  }
+
+  choiceClicked(playerChoice) {
+    const computerChoice = this.getRandomMove();
+    const result = this.determineWinner(playerChoice, computerChoice);
+    this.setState({
+      playerChoice,
+      computerChoice,
+      result,
+    });
+  }
+
+  determineWinner(playerChoice, computerChoice) {
+    if(playerChoice === computerChoice) {
+      this.setState((prevState) => ({
+        tieTally: prevState.tieTally + 1,
+      }));
+      return("It's a tie!");
+    }
+    if(
+      (playerChoice === "rock" && computerChoice === "scissors") ||
+      (playerChoice === "scissors" && computerChoice === "paper") ||
+      (playerChoice === "paper" && computerChoice === "rock") 
+      ) {
+        this.setState((prevState) => ({
+          playerTally: prevState.playerTally + 1,
+        }));
+        return "You win!";
+      } else {
+        this.setState((prevState) => ({
+          computerTally: prevState.computerTally + 1,
+        }));
+        return "Computer wins!";
+      }
+    }
+
+  render() {
+      return (
+        <div className = "App">
+          <h1>Rock Paper Scissors</h1>
+          <h2>by Stefano Farro</h2>
+
+          <div className = "choices">
+            {choices.map((choice) => (
+              <button
+                key={choice}
+                onClick ={() => this.choiceClicked(choice)}
+              >
+                {choice}
+              </button>
+            ))}
+          </div>
+          <div className = "result">
+            {(this.state.playerChoice && (
+              <div>
+              <p>Your choice: {this.state.playerChoice}</p>
+              <p>Computer's choice: {this.state.computerChoice}</p>
+              <p>Result: {this.state.result}</p>
+              </div>
+            ))}
+          </div>
+
+          <div className = "tally">
+              <div>
+              <p>Player Wins: {this.state.playerTally}</p>
+              <p>Computer Wins: {this.state.computerTally}</p>
+              <p>Tied Games: {this.state.tieTally}</p>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
 
 export default App;
